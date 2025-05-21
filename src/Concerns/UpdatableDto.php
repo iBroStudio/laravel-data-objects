213c72e407ed589681ee8cc63d3432e91c582bb9
Yann FREPPEL <yann@ibro.studio>
2025-05-21 06:55:45 +0200

    add UpdatableDto trait

---
+++ src/Concerns/UpdatableDto.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace IBroStudio\DataObjects\Concerns;
+
+/**
+ * @method static from(array $array)
+ * @method all()
+ */
+trait UpdatableDto
+{
+    public function updateDto(array $data): self
+    {
+        return self::from([...$this->all(), ...$data]);
+    }
+}
