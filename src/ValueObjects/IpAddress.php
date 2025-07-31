ff21f550a0415d07961d73ffc38502d2756dfd4b
Yann FREPPEL <yann@ibro.studio>
2025-07-31 16:21:26 +0200

    improve ip address validation

--- src/ValueObjects/IpAddress.php
+++ src/ValueObjects/IpAddress.php
@@ -6,12 +6,22 @@ namespace IBroStudio\DataObjects\ValueObjects;
 
 use Darsyn\IP\Version\Multi;
 use Exception;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\ValidationException;
 
 final class IpAddress extends ValueObject
 {
     public function __construct(mixed $value)
     {
+        $validator = Validator::make(
+            ['ip' => $value],
+            ['ip' => 'ip']
+        );
+
+        if ($validator->stopOnFirstFailure()->fails()) {
+            throw ValidationException::withMessages(['This is not a valid IP address.']);
+        }
+
         try {
             $multi = Multi::factory($value);
         } catch (Exception $e) {
